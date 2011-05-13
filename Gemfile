94c5bed883b600e09e57c2b1485f24ec6acd4459
Kouhei Sutou <kou@clear-code.com>
2011-05-14 01:19:45 +0900

    add generated Rails 3 application.

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.beta'
+
+# Bundle edge Rails instead:
+# gem 'rails',     :git => 'git://github.com/rails/rails.git'
+
+
+
+# Asset template engines
+gem 'json'
+gem 'sass'
+gem 'coffee-script'
+gem 'uglifier'
+
+gem 'jquery-rails'
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# To use debugger
+# gem 'ruby-debug'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlAdmin::Application.config.secret_token = '03d8a88fcf32075a807439be27431d8d106a261a0d5331180e79e92efa02ba2e4fdf2d682fdebc92feb9ddb759ddb9edf2d1276d8cb21865507497b9f8057994'
