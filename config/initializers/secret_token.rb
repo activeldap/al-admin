6c9f750eb2875d29698e847fa48270ca9c6d7831
Kouhei Sutou <kou@clear-code.com>
2011-05-14 01:38:34 +0900

    generate secret token at the first run.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,13 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-AlAdmin::Application.config.secret_token = '03d8a88fcf32075a807439be27431d8d106a261a0d5331180e79e92efa02ba2e4fdf2d682fdebc92feb9ddb759ddb9edf2d1276d8cb21865507497b9f8057994'
+
+secret_token_file = Rails.root + "config" + "secret_token.txt"
+unless secret_token_file.exist?
+  secret_token_file.open("w") do |file|
+    file.puts(ActiveSupport::SecureRandom.hex(64))
+  end
+end
+secret_token = secret_token_file.read.strip
+
+AlAdmin::Application.config.secret_token = secret_token
